78e3400f36c8f0ffa77ffd97be02cc5bda094b4b
Collin Seebeck <collin.seebeck@gmail.com>
2021-06-29 14:13:41 -0400

    Updated main_bank.py to calculate statistics on changes in profit/losses over time

--- PyBank/main_bank.py
+++ PyBank/main_bank.py
@@ -21,11 +21,22 @@ with open(budget_csv, encoding='utf-8') as csvfile:
         net_total +=int(row[1])
         profit_loss.append(int(row[1]))
 
+total_change=0
 #Calculate the changes over the entire period
 for m in range(1,len(profit_loss)):
-    #print(f"Index {m}  Value {profit_loss[m]}")
     pl_change.append(profit_loss[m]-profit_loss[m-1])
+    total_change+=profit_loss[m]-profit_loss[m-1]
 
+#Calculate average change, max change and min change
+avg_change=round(total_change/len(pl_change),2)
+max_increase=max(pl_change)
+min_increase=min(pl_change)
+
+#Print Statements
+#print(Total Months:)
 print(f"Total: ${net_total}")
+print(f"Average Change: ${avg_change}")
+print(f"Greatest Increase in Profits: ${max_increase}")
+print(f"Greatest Decrease in Profits:${min_increase}")
 
 
