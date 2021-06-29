3e01222f4d1082484a486520006f9531d89276e0
Collin Seebeck <collin.seebeck@gmail.com>
2021-06-29 13:57:34 -0400

    Updated main_bank.py to calculate changes in Profit/Losses

--- PyBank/main_bank.py
+++ PyBank/main_bank.py
@@ -8,7 +8,7 @@ budget_csv=os.path.join('..', 'Resources', 'budget_data.csv')
 #Initialize variables
 net_total=0
 profit_loss=[]
-useless=0
+pl_change=[]
 
 #Open CSV file and begin working with data
 with open(budget_csv, encoding='utf-8') as csvfile:
@@ -17,10 +17,15 @@ with open(budget_csv, encoding='utf-8') as csvfile:
     next(csvreader)
     #Loop Through rest of rows
     for row in csvreader:
+        #Calculate Total Change 
         net_total +=int(row[1])
+        profit_loss.append(int(row[1]))
 
-print(net_total)
+#Calculate the changes over the entire period
+for m in range(1,len(profit_loss)):
+    #print(f"Index {m}  Value {profit_loss[m]}")
+    pl_change.append(profit_loss[m]-profit_loss[m-1])
 
-
+print(f"Total: ${net_total}")
 
 
