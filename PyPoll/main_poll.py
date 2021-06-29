0c3f2d40f671bed9b1624d5026aca59a7248632d
Collin Seebeck <collin.seebeck@gmail.com>
2021-06-29 16:38:47 -0400

    Updated main_poll.py to find candidate list

--- PyPoll/main_poll.py
+++ PyPoll/main_poll.py
@@ -7,6 +7,7 @@ election_csv=os.path.join('..', 'Resources', 'election_data.csv')
 
 #Initialize variables
 vote_count=0
+all_votes=[]
 
 with open(election_csv, encoding='utf-8') as csvfile:
     csvreader=csv.reader(csvfile,delimiter=',')
@@ -15,5 +16,32 @@ with open(election_csv, encoding='utf-8') as csvfile:
     #Loop Through rest of rows
     for row in csvreader:
         vote_count+=1
+        all_votes.append(row[2])
 
-print(vote_count)
+#Create a list of candidates by removing duplicates using set function
+candidates_set=set(all_votes)
+#Convert set to list 
+candidates_list=list(candidates_set)
+
+individual_votes=[]
+#Count votes for each candidate
+#for num in range(len(candidates_list)):
+#    candidate_votes=0
+#    candidate_votes=all_votes.count(candidates_list[num]) 
+#    individual_votes.append(candidate_votes)
+
+#Print Statements 
+print("Election Results")
+print("------------------------")
+print(f"Total Votes: {vote_count}")
+print("------------------------")
+print(list(candidates_list))
+#print(list(individual_votes))
+
+#Khan: 63.000% (2218231)
+#Correy: 20.000% (704200)
+#Li: 14.000% (492940)
+#O'Tooley: 3.000% (105630)
+#-------------------------
+#Winner: Khan
+#-------------------------
