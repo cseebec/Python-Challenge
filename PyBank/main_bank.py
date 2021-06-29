162d5265f3cb91f0efb7be623ea68275b3baa4f9
Collin Seebeck <collin.seebeck@gmail.com>
2021-06-29 14:39:21 -0400

    Updated main_bank.py to find the months with the greatest increase and decrease

--- PyBank/main_bank.py
+++ PyBank/main_bank.py
@@ -9,6 +9,8 @@ budget_csv=os.path.join('..', 'Resources', 'budget_data.csv')
 net_total=0
 profit_loss=[]
 pl_change=[]
+date_list=[]
+total_months=0
 
 #Open CSV file and begin working with data
 with open(budget_csv, encoding='utf-8') as csvfile:
@@ -17,12 +19,16 @@ with open(budget_csv, encoding='utf-8') as csvfile:
     next(csvreader)
     #Loop Through rest of rows
     for row in csvreader:
-        #Calculate Total Change 
+        #Calculate the Net Total 
         net_total +=int(row[1])
         profit_loss.append(int(row[1]))
+        date_list.append(row[0])
+        #Assumption made each row in the csv file is a different month
+        total_months+= 1
 
 total_change=0
 #Calculate the changes over the entire period
+#Start at 1 so the first entry is the second value minus the first value
 for m in range(1,len(profit_loss)):
     pl_change.append(profit_loss[m]-profit_loss[m-1])
     total_change+=profit_loss[m]-profit_loss[m-1]
@@ -32,11 +38,16 @@ avg_change=round(total_change/len(pl_change),2)
 max_increase=max(pl_change)
 min_increase=min(pl_change)
 
+#Find the indexes of max and min increase
+#Have to add 1 to the index because pl_change starts at the second index of profit_loss
+mx=pl_change.index(max_increase)+1
+mn=pl_change.index(min_increase)+1
+
 #Print Statements
-#print(Total Months:)
+print(f"Total Months: {total_months}")
 print(f"Total: ${net_total}")
 print(f"Average Change: ${avg_change}")
-print(f"Greatest Increase in Profits: ${max_increase}")
-print(f"Greatest Decrease in Profits:${min_increase}")
+print(f"Greatest Increase in Profits: {date_list[mx]} (${max_increase})")
+print(f"Greatest Decrease in Profits: {date_list[mn]} (${min_increase})")
 
 
